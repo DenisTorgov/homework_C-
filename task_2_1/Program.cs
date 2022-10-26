e5debbafe75e61da708b9892ceb9e5c67ef3d6fc
Denis.Torgov <deniska-3000@bk.ru>
2022-10-27 00:08:21 +0300

    tasks_2_1 complited

---
+++ task_2_1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. Не использовать строки для расчета.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+int promt(string msg)
+{
+	Console.WriteLine(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int numb = promt("Введите трехзначное целое число > ");
+int numb_2nd = numb % 100;
+numb_2nd = numb_2nd / 10;
+Console.WriteLine($"Вторая цифрв числа = {numb_2nd}");
