be3c72ba69874ecc7a37e87ef2c7cdc0ca110489
Denis.Torgov <deniska-3000@bk.ru>
2022-10-27 00:47:58 +0300

    tasks_2_3 complited

---
+++ task_2_3/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 3: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. Не использовать строки для расчета.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 326792 -> 6
+
+
+int randnumb()
+{
+	int numb_gen = new Random().Next();
+	Console.WriteLine($"Случайное сгенерированное целое число {numb_gen}");
+ 	return numb_gen;
+}
+
+int numb = randnumb();
+if (numb > 99) 
+{
+	int length = 0;
+	int cutnumb = numb;
+	while (cutnumb > 0)
+	{
+		cutnumb = cutnumb / 10;
+		length++;	
+	}
+	int result = Convert.ToInt32(Math.Floor(numb / (Math.Pow(10, (length - 3)))));
+    result = result % 10;
+	Console.WriteLine($"Третья цифра числа равна {result}");
+}
+else
+{
+	Console.WriteLine("Третьей цифры нет");
+}
+
+
+
