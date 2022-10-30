cd2ac774baff0051222c6146b77083e5a58aa6a9
Denis.Torgov <deniska-3000@bk.ru>
2022-10-31 01:23:47 +0300

    tasks_3_3 complited

---
+++ task_3_3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 3
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+int prompt (string msg)
+{
+    Console.WriteLine(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int cube (int numb)
+{
+    return numb*numb*numb;
+}
+
+int n = prompt("Введите целое число");
+for (int i = 1; i < n; i++)
+{
+    Console.Write($"{cube(i)}, ");
+}
+Console.Write($"{cube(n)}");
