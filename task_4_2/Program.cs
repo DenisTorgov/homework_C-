b4e4c7604d5cbcd44596087b810dec8a802ad667
Denis.Torgov <deniska-3000@bk.ru>
2022-11-18 20:38:32 +0300

    tasks_4_2 complited

---
+++ task_4_2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Prompt(string msg)
+{
+    Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int NumSum(int a)
+{
+    int x = 0;
+    while (a > 0)
+    {
+        x = x + a % 10;
+        a = a / 10;
+    }
+    return x;
+}
+
+int numb = Prompt("Введите число больше 0 -> ");
+int sum = NumSum(numb);
+Console.WriteLine($"Сумма цифр введенного числа равна: {sum}");
