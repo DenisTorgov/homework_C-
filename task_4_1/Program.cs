1ea92aa8c443d15fb2db03918407c50aeb683340
Denis.Torgov <deniska-3000@bk.ru>
2022-11-18 20:26:27 +0300

    tasks_4_1 complited

---
+++ task_4_1/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 1: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int Prompt(string msg)
+{
+    Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+double NumPower(int a, int n)
+{
+    double ans = 1;
+
+    while (n > 0)
+    {
+        if (n % 2 == 0)
+        {
+            a = a*a;
+            n = n / 2;
+        }
+        else
+        {
+            ans = ans * a;
+            n = n - 1;
+        } 
+
+    }
+    return ans;
+}
+
+int numb = Prompt("Введите число больше 0 -> ");
+int stepen = Prompt("Введите степень больше 0 -> ");
+double x = NumPower(numb, stepen);
+Console.WriteLine($"Число {numb} в степени {stepen} равно: {x}");
